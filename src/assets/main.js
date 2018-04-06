fe99cf5443cd9388c03c96e9e51385f0e7f18298
Raj Vemula <vemular2015@gmail.com>
2018-04-06 17:07:14 -0400

    changed setHiddenFields function to change answer to 4 characters

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,7 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 let message = document.getElementById("message");
+
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
@@ -8,11 +9,17 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields(){
-        answer.value = Math.floor(Math.random() * 9999);
-        attempt.value = 0;
+
+    answer = Math.floor(Math.random() * 9999).toString();
+    if(answer.length <4){
+        for(let i = 0; i<answer.length; i++){
+            answer = "0"+ answer;
+        }
+    }
+    attempt.value = 0;
 
 }
 
-function setMessage(input){
- message.innerHTML = input;
+function setMessage(message){
+ message.innerHTML = message;
 }
