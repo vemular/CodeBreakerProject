9b589b9e08c9c507c745692bf9d8124f97eb70be
Raj Vemula <vemular2015@gmail.com>
2018-04-06 16:51:22 -0400

    again

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,5 +8,5 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields(){
-    answer = Math.floor(Math.random() * 9999);
+    answer = Math.floor((Math.random() * 9999)+0);
 }
