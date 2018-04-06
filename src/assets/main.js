e2aa524c0485e26924a2cfdaf2dde2ab1bc3571e
Raj Vemula <vemular2015@gmail.com>
2018-04-06 17:02:36 -0400

    added setMessage fucntion

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,6 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
-
+let message = document.getElementById("message");
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
@@ -8,5 +8,11 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields(){
-    answer.value = Math.floor(Math.random() * 9999);
+        answer.value = Math.floor(Math.random() * 9999);
+        attempt.value = 0;
+
+}
+
+function setMessage(input){
+ message.innerHTML = input;
 }
