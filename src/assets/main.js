196612a7d4e4695382679ca431acad7bf5a8297f
Raj Vemula <vemular2015@gmail.com>
2018-04-06 16:47:07 -0400

    changed setHiddenFields again

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,7 +1,6 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
-const MAX = 9999;
-const MIN = 0;
+
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
@@ -9,5 +8,5 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields(){
-    let answer = Math.floor(Math.random() * 9999);
+    this.answer = Math.floor(Math.random() * 9999);
 }
